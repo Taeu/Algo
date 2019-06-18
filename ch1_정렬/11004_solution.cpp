e86969498e347b522dc4c832b19458cdc7024660
Taeu <specia1ktu@gmail.com>
2019-06-18 21:27:12 +0900

    do it as medium sort

--- ch1_정렬/11004_solution.cpp
+++ ch1_정렬/11004_solution.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #pragma warning(disable : 4996)
 int v[5000000];
 // https://en.wikipedia.org/wiki/Median_of_medians#Algorithm 이거 공부해보기
+// https://www.acmicpc.net/source/12995642
+// medium 으로 하면 되는 것 같네..;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
